93a48c0334bc6ca59126f9d9fe4a555ab8b75570
cebotari.dan <cebotari.dan01@gmail.com>
2024-12-10 16:02:45 +0200

    Added review insertion based on logged in status, improved UI overall

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -8,35 +8,4 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
-  images: string[] = [
-    'https://images.hdqwalls.com/wallpapers/i-love-coffee.jpg',
-    'https://w0.peakpx.com/wallpaper/531/501/HD-wallpaper-coffee-espresso-latte-art-cup-food.jpg',
-  ];
-  currentImage = 0;
-  imageInterval: any;
-  messageOfTheDay: string = "Start your day with a cup of joy. Every sip counts!";
-  specialOffer: string | null = "Get 20% off on all coffee products this weekend only!";
-
-  ngOnInit() {
-    this.startAutoSwitch();
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.imageInterval);
-  }
-
-  prevImage() {
-    this.currentImage = (this.currentImage === 0) ? this.images.length - 1 : this.currentImage - 1;
-  }
-
-  nextImage() {
-    this.currentImage = (this.currentImage === this.images.length - 1) ? 0 : this.currentImage + 1;
-  }
-
-  startAutoSwitch() {
-    this.imageInterval = setInterval(() => {
-      this.nextImage();
-    }, 15000);
-  }
-}
+export class HomePageComponent {}
